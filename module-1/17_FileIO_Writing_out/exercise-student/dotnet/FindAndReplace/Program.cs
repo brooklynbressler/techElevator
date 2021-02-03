31c49c8b2232a8d033ec2f965223d27e7f031c27
brooklyn bressler <brooklynbressler@gmail.com>
2021-02-03 16:32:36 -0500

    Week 4 Day 2 Exercises

--- module-1/17_FileIO_Writing_out/exercise-student/dotnet/FindAndReplace/Program.cs
+++ module-1/17_FileIO_Writing_out/exercise-student/dotnet/FindAndReplace/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.IO;
 
 namespace FindAndReplace
 {
     public class Program
     {
-		public static void Main(string[] args)
-		{
-            Console.WriteLine("Hello World!");
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is the search word?");
+            string userSearchWord = Console.ReadLine();
+
+            Console.WriteLine("What is the replacement word?");
+            string userReplacementWord = Console.ReadLine();
+
+            Console.WriteLine("What is the source file?");
+            string userSourceFile = Console.ReadLine();
+
+            Console.WriteLine("What is the destination file?");
+            string userDestinationFile = Console.ReadLine();
+
+            //C:\Users\Student\workspace\bbressler-c\module-1\17_FileIO_Writing_out\exercise-student\dotnet\FindAndReplace\werdz.txt
+            //C:\Users\Student\workspace\bbressler-c\module-1\17_FileIO_Writing_out\exercise-student\dotnet\FindAndReplace\werdz-replace.txt
+
+            try
+            {
+                // Open the existing file with the typo using a StreamReader
+                using (StreamReader sr = new StreamReader(userSourceFile))
+                {
+                    // Open a StreamWriter where we will output the file
+                    using (StreamWriter sw = new StreamWriter(userDestinationFile, false))
+                    {
+                        // For each line in the input file, read it in
+                        while (!sr.EndOfStream)
+                        {
+                            // Read an individual line
+                            string line = sr.ReadLine();
+
+                            // Replace the occurence of the word langauge with language
+                            string fixedLine = line.Replace(userSearchWord, userReplacementWord);
+
+
+                            // Write the new line to the output file
+                            sw.WriteLine(fixedLine);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("couldnt do it, sorry");
+
+            }
         }
     }
 }
--- module-1/17_FileIO_Writing_out/exercise-student/dotnet/FizzWriter/Program.cs
+++ module-1/17_FileIO_Writing_out/exercise-student/dotnet/FizzWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FizzWriter
 {
@@ -6,7 +7,49 @@ namespace FizzWriter
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is the destination file?");
+            string destinationFile = Console.ReadLine();
+
+            //C:\Users\Student\workspace\bbressler-c\module-1\17_FileIO_Writing_out\exercise-student\dotnet\FizzWriter\FizzDestination.txt
+
+            try
+            {
+                // Open a StreamWriter where we will output the file
+                using (StreamWriter sw = new StreamWriter(destinationFile))
+                {
+
+                    for (int i = 1; i <= 300; i++)
+                    {
+
+                        if (i % 3 == 0 && i % 5 == 0)
+                        {
+                            sw.WriteLine("FizzBuzz");
+                        }
+                        else if (i % 5 == 0 || i.ToString().Contains("5"))
+                        {
+                            sw.WriteLine("Buzz");
+                        }
+                        else if (i % 3 == 0 || i.ToString().Contains("3"))
+                        {
+                            sw.WriteLine("Fizz");
+                        }
+                        else
+                        {
+                            sw.WriteLine(i);
+                        }
+
+
+                    }
+
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("couldnt do it, sorry");
+
+            }
         }
+
     }
 }

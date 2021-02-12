0937d5eb37544d90a53e3f27fde7e3dabf127338
brooklyn bressler <brooklynbressler@gmail.com>
2021-02-12 10:23:57 -0500

    Brooklyn Bressler Assessment

---
+++ Assessments/module-1/student-assessment/dotnet/Assessment/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment
+{
+    public class Car
+    {
+        //    * `year`: indicates the year that the car was manufactured.
+        //    * `make`: indicates the make of the car.
+        //    * `is classic car`: indicates if the car is a classic car.
+        //    * `age`: indicates the age (in years) of the car from the current year.
+
+        public int Year { get; set; }
+        public int Age { get; set; }
+        public string Make { get; set; }
+        public bool IsClassicCar { get; set; }
+        
+        public Car(int year, string make, bool isClassicCar)
+        {
+
+            Year = year;
+            Make = make;
+            IsClassicCar = isClassicCar;
+
+        }
+    
+        public bool ECheck(int yearToCheck)
+        {
+            bool isECheckNeeded = false;
+
+            if(yearToCheck % 2 == 0 || yearToCheck % 2 != 0)
+            {
+                isECheckNeeded = true;
+            }
+            else if(yearToCheck < 4 || yearToCheck > 25 || IsClassicCar == true)
+            {
+                isECheckNeeded = false;
+            }
+
+            return isECheckNeeded;
+        }
+
+        public string ToString(int year, string make)
+        {
+            string descriptionOfCar = $"CAR - {year} - {make}";
+
+            return descriptionOfCar;
+        }
+    
+    
+    }   
+}
--- Assessments/module-1/student-assessment/dotnet/Assessment/Program.cs
+++ Assessments/module-1/student-assessment/dotnet/Assessment/Program.cs
@@ -1,13 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Assessment
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+
+
+        static void Main(string[] args, int year, string make, bool isClassicCar)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+
+            Car car = new Car(year, make, isClassicCar);
+
+            List<Car> listOfCars = new List<Car>();
+
+            string directory = Environment.CurrentDirectory;
+            string filename = "CarInput.csv";
+            string fullPath = Path.Combine(directory, filename);
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(fullPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+
+                        string line = sr.ReadLine();
+                        string[] words = line.Split(',');
+
+                        year = int.Parse(words[0]);
+                        make = words[2];
+                        isClassicCar = bool.Parse(words[3]);
+
+                        if(year == 1965)
+                        {
+
+                            listOfCars.Add(car);
+
+                        }
+                        else if(year == 2019)
+                        {
+
+                            listOfCars.Add(car);
+                        
+                        }
+                        else if(year == 1920)
+                        {
+
+                            listOfCars.Add(car);
+                        
+                        }
+                        else if(year == 1970)
+                        {
+                            listOfCars.Add(car);
+                        }
+                        else if (year == 2018)
+                        {
+                            listOfCars.Add(car);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                    }
+
+
+                }
+
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("There was an error reading the input file");
+            }
+
+
+            Console.WriteLine($"The sum for the ages of all the cars is ): ");
+        
+        
+        
+        
         }
     }
 }

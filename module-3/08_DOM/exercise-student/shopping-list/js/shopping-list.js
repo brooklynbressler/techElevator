528f5ff195a78989924e7b369436fa8e11efb605
brooklyn bressler <brooklynbressler@gmail.com>
2021-03-18 15:26:07 -0400

    Week 9 Day 3

--- module-3/08_DOM/exercise-student/shopping-list/js/shopping-list.js
+++ module-3/08_DOM/exercise-student/shopping-list/js/shopping-list.js
@@ -1,31 +1,69 @@
 // add pageTitle
 
+const pageTitle = "My Shopping List";
+
 // add groceries
+
+const groceries = ["Vegan Milk", "Vegan Cheese", "Vegan Creamer", "Organic Banana Bunch", "Wheat Bread", "Salad Mix", "Frozen Vegan Pizza", "Cat Litter", "Cat Treats", "Cat Food"];
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+
+function setPageTitle() 
+{
+  const title = document.getElementById("title");
+
+  title.innerText = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+
+function displayGroceries() 
+{
+  const addingGroceries = document.getElementById("groceries");
+
+  groceries.forEach(grocery => 
+  {
+    const groceryList = document.createElement("li");
+
+    groceryList.classList.add("grocery");
+
+    groceryList.innerText = grocery;
+
+    addingGroceries.insertAdjacentElement("beforeend", groceryList);    
+  });
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+
+function markCompleted() 
+{
+  const listArray = Array.from(document.querySelectorAll(".grocery"));
+
+  listArray.forEach(listItem => 
+  {
+    listItem.classList.add("completed");
+  });
+}
 
 setPageTitle();
 
 displayGroceries();
 
 // Don't worry too much about what is going on here, we will cover this when we discuss events.
+
 document.addEventListener('DOMContentLoaded', () => {
+
   // When the DOM Content has loaded attach a click listener to the button
+
   const button = document.querySelector('.btn');
+  
   button.addEventListener('click', markCompleted);
 });
